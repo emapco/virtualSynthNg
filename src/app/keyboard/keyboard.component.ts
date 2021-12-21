b84f8d403a7d27f0e3a0376d5802a0363c62750c
Emmanuel Cortes <polosetroc@gmail.com>
2021-12-21 10:15:28 -0800

    Added simple animation to synthesizer keyboard when played.

--- src/app/keyboard/keyboard.component.ts
+++ src/app/keyboard/keyboard.component.ts
@@ -10,14 +10,15 @@ import {PlayNoteService} from "../shared/play-note.service";
 export class KeyboardComponent {
   // boolean to prevent touch event from triggering touchstart and mousedown thus playing note twice
   private previousEventTouch: boolean = false;
-  constructor(private playNoteSer: PlayNoteService) {
-  }
+
+  constructor(private playNoteSer: PlayNoteService) { }
 
   @HostListener('document:keydown', ['$event'])  // listener must be in a component
   handleKeyboardEvent(event: KeyboardEvent) {
-    let parseEventKey = event.key.toUpperCase();
-    if (Object.keys(this.playNoteSer.noteFrequencies).includes(parseEventKey)) {
-      this.playNoteSer.playNotes(parseEventKey);
+  let key = this.isSupportedKey(event);
+    if (key) {
+      this.playNoteSer.playNotes(key);
+      this.animateKeyPress(key);
     }
   }
 
@@ -28,8 +29,53 @@ export class KeyboardComponent {
       let element = event.target as HTMLElement;
       if (element.tagName === "KBD") {
         this.playNoteSer.playNotes(element.innerText);
+        this.animateKeyPress(element.innerText);
       }
     }
     this.previousEventTouch = (event.type === 'touchstart');
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  @HostListener('touchend', ['$event'])
+  @HostListener('mouseup', ['$event'])
+  handleKeyClickTouchRelease(event: TouchEvent | MouseEvent | KeyboardEvent) {
+    if ("key" in event) {
+      let key = this.isSupportedKey(event);
+      if (key) {
+        let element = document.getElementById(key);
+        // @ts-ignore
+        this.animateKeyRelease(element);
+      }
+    } else {
+      let element = event.target as HTMLElement;
+      if (element.tagName === "KBD") {
+        this.animateKeyRelease(element);
+      }
+    }
+  }
+
+  animateKeyPress(key: string) {
+    let element = document.getElementById(key);
+    if (element != null) {
+      element.style.backgroundColor = "gray";
+      element.style.color = "white";
+    }
+  }
+
+  animateKeyRelease(element: HTMLElement) {
+    let bgColor = "rgb(33, 37, 41)";
+    let color = "white";
+    // @ts-ignore
+    if (element.parentElement.id === "white-keys-container") {
+      bgColor = "white";
+      color = "black";
+    }
+    element.style.backgroundColor = bgColor;
+    element.style.color = color;
+  }
+
+  isSupportedKey(event: KeyboardEvent) {
+    let key = event.key.toUpperCase();
+    return (Object.keys(this.playNoteSer.noteFrequencies).includes(key)) ? key : '';
   }
 }
